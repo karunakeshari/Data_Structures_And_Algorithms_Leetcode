75518a3fc60e81e5d441ca7b118b912d27c3c02d
karunakeshari <karunakeshari884@gmail.com>
2022-08-15 11:36:26 +0530

    3 Sum

---
+++ Array/3 Sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+       //Two Pointer Approach
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<nums.size()-2;i++){
+            if((i==0) || (i>0 && nums[i]!=nums[i-1])){
+                int low=i+1,high=nums.size()-1;
+                int sum=0-nums[i];
+                while(low<high){
+                    if(nums[low]+nums[high]==sum){
+                        vector<int> temp;
+                        temp.push_back(nums[low]);
+                        temp.push_back(nums[high]);
+                        temp.push_back(nums[i]);
+                        ans.push_back(temp);
+                        while(low<high && nums[low]==nums[low+1]){
+                            low++;
+                        }
+                        while(low<high && nums[high]==nums[high-1]){
+                            high--;
+                        }
+                        low++;
+                        high--;
+                    }
+                    else if(nums[low]+nums[high]<sum){
+                        low++;
+                    }  
+                    else{
+                        high--;
+                    } 
+                }
+            }
+        }
+        return ans;
+    }
+};
+//Time Complexity- O(N^2)
+//Space Complexity- O(1)
